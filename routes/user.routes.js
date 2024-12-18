81ac28d0e9bd745ee54176db99f7fe85c9881755
abanoub-refaat <abanoubref3at18@gmail.com>
2024-12-18 22:56:53 +0200

    Add user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,4 +7,14 @@ router.post("/signup", userController.signup);
 
 router.post("/login", userController.login);
 
+router.get("/me", userController.getMe);
+
+router.patch("/update", userController.updateUser);
+
+router.delete("/delete", userController.deleteUser);
+
+router.get("/:id", userController.getUser);
+
+router.get("/", userController.getAllUsers);
+
 module.exports = router;
